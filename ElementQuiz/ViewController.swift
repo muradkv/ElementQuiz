2b76312d37162acc6ade8796e66a03d72c01c67d
murad <muradkv@gmail.com>
2019-07-08 23:32:58 +0300

    Add outlets and actions

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func showAnswer(_ sender: UIButton) {
+    }
+    
+    @IBAction func goToNextElement(_ sender: UIButton) {
+    }
 }
 
